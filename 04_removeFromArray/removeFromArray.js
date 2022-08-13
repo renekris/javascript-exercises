a2aabd3dcc290faf3496b24cbb13dec389ffc9f9
Renekris <renekrispohlak@gmail.com>
2022-08-13 11:22:03 +0300

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let argsArray = args;
+    let arrayRemove = args.shift();
 
+    for (const arg of argsArray) {
+        if (arrayRemove.indexOf(arg) >= 0) arrayRemove.splice(arrayRemove.indexOf(arg), 1);
+    }
+    return arrayRemove;
 };
 
 // Do not edit below this line
