9b7ca993b2ed6cbd812066b58bc8d6c3e4024878
Renekris <renekrispohlak@gmail.com>
2022-08-13 08:11:27 +0300

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let fullString = "";
+    if (number >= 0) {
+        for (let i = 0; i < number; i++) {
+            fullString += string;
+        }
+        return fullString;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
