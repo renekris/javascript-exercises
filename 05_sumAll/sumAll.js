36ed499909da3ed1775bdb0ec689ffc65dcd589d
Renekris <renekrispohlak@gmail.com>
2022-08-14 10:28:09 +0300

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(numFrom, numTo) {
+    let sumFinal = 0;
+    if (
+        numFrom.constructor === Number 
+        && numTo.constructor === Number
+        && numFrom >= 0 
+        && numTo >= 0
+        ) {
+        if (numFrom > numTo) [numFrom, numTo] = [numTo, numFrom]
 
+        for (numFrom; numFrom <= numTo; numFrom++) {
+            sumFinal += numFrom;
+        }
+
+        return sumFinal;
+    } else return "ERROR";
 };
 
 // Do not edit below this line
