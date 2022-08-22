7a37e70eeabebff40a0a8eef0c9cf8baddc7294c
Renekris <renekrispohlak@gmail.com>
2022-08-22 22:44:19 +0300

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = nth => {
+    if (nth <= 0) return 'OOPS';
 
+    let fibArray = [1, 1];
+    for (let index = 0; fibArray.length < nth; index++) {
+        fibArray.push(fibArray[index] + fibArray[index + 1]);
+    }
+    return fibArray.pop();
 };
 
 // Do not edit below this line
