ecab51700de9d6734d7e929f885e2a32009862b1
Renekris <renekrispohlak@gmail.com>
2022-08-22 22:20:38 +0300

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = string => {
+    string = string.toLowerCase().replace(/[^a-z]/g, '');
+    const reverseString = string.split('').reverse().join('')
 
+    return reverseString === string
 };
 
 // Do not edit below this line
