6ea523666839c9b6f4413da9ddfbf5253153a903
Renekris <renekrispohlak@gmail.com>
2022-08-22 22:57:46 +0300

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = obj => {
+    let bookTitles = [];
+    obj.forEach(element => {
+        bookTitles.push(element['title']);
+        // bookTitles.push(element.title); Or like this
+    });
+    return bookTitles;
 };
 
 // Do not edit below this line
