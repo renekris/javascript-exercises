39b03cc4c128370e4f24f6ad4b205c851bc10a30
Renekris <renekrispohlak@gmail.com>
2022-08-23 00:44:34 +0300

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = obj => {
+    // obj.reduce((oldest, person, index, array) => {
+    //     array.sort((a, b) => {
 
+    //     })
+    //     return person.yearOfDeath - person.yearOfBirth
+    // }, 0);
+    const oldest = obj.sort((a, b) => {
+        if (typeof a.yearOfDeath === 'undefined') { a.yearOfDeath = new Date().getFullYear(); }
+        if (typeof b.yearOfDeath === 'undefined') { b.yearOfDeath = new Date().getFullYear(); }
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        let oldest = aAge > bAge ? -1 : 1;
+        return oldest;
+    });
+    return oldest.shift();
 };
 
 // Do not edit below this line
